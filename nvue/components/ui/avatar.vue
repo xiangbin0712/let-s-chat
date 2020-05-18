88580275ccc985457e7bed1d478417a6aa16c56b
xiangbin <xiangbin0712@gmail.com>
2020-05-18 09:57:52 +0800

    添加聊天列表

---
+++ nvue/components/ui/avatar.vue
@@ -0,0 +1,25 @@
+<template>
+	<image :src="src" :style="_style" mode="widthFix"></image>
+</template>
+
+<script>
+export default {
+	props: {
+		src: String,
+		size: {
+			type: [String, Number],
+			default: 90
+		}
+	},
+	data() {
+		return {};
+	},
+	computed:{
+		_style(){
+			return `width:${this.size}rpx;height:${this.size}rpx`
+		}
+	}
+};
+</script>
+
+<style></style>
---
+++ nvue/components/ui/chat-list.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="chat-list  flex flex-row align-center ">
+		<view class="avatar-container ml-3"><image class="avatar" src="/static/logo.png" mode=""></image></view>
+		<view class="right pl-2 pr-3  flex  justify-center border-bottom flex-1 ">
+			<view class="right-container flex justify-center">
+				<view class="flex flex-row  justify-between ">
+					<text>123132</text>
+					<text class="font-sm text-muted">time</text>
+				</view>
+				<text class="chat-info text-muted text-ellipsis font-sm">很骄傲和哈哈哈哈很骄傲和哈哈哈哈很骄傲和哈哈哈哈很骄傲和哈哈哈哈哈啊哈哈哈哈哈哈啊哈哈哈哈哈哈啊哈哈哈哈哈哈啊哈哈哈哈哈</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script></script>
+
+<style scoped>
+
+.avatar {
+	width: 90rpx;
+	height: 90rpx;
+}
+
+.right {
+	/* height: 130rpx; */
+	padding-bottom: 30rpx;
+	padding-top: 30rpx;
+
+}
+
+.right-container {
+	height: 90rpx;
+}
+</style>
